c3a93ca933d64800578a82c8c56a561cf2ed783a
huong huong <huongnguyen08.cv@gmail.com>
2017-10-11 21:24:25 +0700

    buoi7

--- buoi6/js/index3.js
+++ buoi6/js/index3.js
@@ -1,7 +1,25 @@
 function getData(){
     var soa = document.getElementsByClassName('soa')
+    //console.log(soa)
     for(var i = 0; i < soa.length; i++){
         console.log(soa[i].innerHTML)
     }
     
 }
+function getData02(){
+    var data = document.getElementsByTagName('p')
+    console.log(data);
+    for(var i = 0; i < data.length; i++){
+        console.log(data[i].innerHTML)
+    }
+}
+
+
+function inSo(so=7){
+    return so;
+}
+
+var b = inSo(9);
+console.log(b);
+
+//console.log(typeof(b))
